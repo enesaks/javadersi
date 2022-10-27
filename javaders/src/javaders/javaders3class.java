009b3bf45d9e898f763ac03cd2dcf3351dc33994
aidata <aidata@10.121.3.185>
2022-10-27 12:24:27 +0300

    as

--- javaders/src/javaders/javaders3class.java
+++ javaders/src/javaders/javaders3class.java
@@ -14,8 +14,9 @@ public class javaders3class {
 				"2. Hangi gezegenin atmosferi küresel ısınma ile yok edilmektedir?",
 				"3. Hangi tür ampul en az enerjiyi tüketir?",
 				"4. İklim değişikliği nedeniyle yılda kaç kişi ölmektedir?",
+				"5. Küresel ısınmaya karşı en savunmasız hayvan hangisidir?"
 				};
-		String[] Cevaplar = {"A","B","C","C"};
+		String[] Cevaplar = {"A","B","C","C","C"};
 		
 		System.out.println(Sorular[0]+"\n A-Cin B-Hindistan C-Amerika D-Japonya");
 		cevap = s.nextLine();
@@ -56,16 +57,44 @@ public class javaders3class {
 		
 		
 		
-		System.out.println(Sorular[2]+"\n A-Akkor Ampul B-Halojen ampul C-Kompakt Floresan Ampul");
+		System.out.println(Sorular[3]+"\n A-1500 B-100.000 C-150.000 D-500.000");
 		cevap = s.nextLine();
 		cevap.toUpperCase();
 		
-		if(Cevaplar[2].equals(cevap)) {
+		if(Cevaplar[3].equals(cevap)) {
 			System.out.println("Doğru cevap");
 			dogrucevap++;
 		}
 		else {
 			System.out.println("Yanlis cevap");
+		}
+		
+		System.out.println(Sorular[4]+"\n A-Kar Leoparı B-Kutup Ayisi C-Deniz Gergedanı D-Tukan");
+		cevap = s.nextLine();
+		cevap.toUpperCase();
+		
+		if(Cevaplar[4].equals(cevap)) {
+			System.out.println("Doğru cevap");
+			dogrucevap++;
+		}
+		else {
+			System.out.println("Yanlis cevap");
+		}
+		
+		
+		
+		
+		
+		
+		
+		switch(dogrucevap){
+			case 5:System.out.println("Mükemmel");
+				break;
+			case 4:System.out.println("Cok iyi");
+				break;
+			default:System.out.println("Kuresel isinma hakinda bilgikerinizi tazelemeniz gerek.");
+			break;	
+		
 		}
 		
 		
